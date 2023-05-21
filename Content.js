49c7cfea4e649f16a52b333d34079d90a9777d63
Daniel Gorbatov <dzgorbatov@gmail.com>
2023-05-21 15:47:05 -0700

    fuck youtube

--- Content.js
+++ Content.js
@@ -8,5 +8,39 @@ function trigger() {
         triggered = true;
     }
     console.log("triggered: " + triggered);
+
+    getCaptions(document);
+    
+
     return triggered;
+}
+
+async function getCaptions(document) {
+    console.log(document.body.innerHTML);
+
+    var regexp = new RegExp(/playerCaptionsTracklistRenderer.*?(youtube.com\/api\/timedtext.*?)"/);
+    var match = regexp.exec(document.body.innerHTML);
+
+    if (!match) {
+        alert ("No captions found");
+        return;
+    }
+
+    var url = regexp.exec(document.body.innerHTML)[1];
+    let decodedUrl = decodeURIComponent(JSON.parse(`"${url}"`));
+    let res = await fetch(decodedUrl.replace("youtube.com/", ""));
+    let textRes = (await res.text()).split("</text>");
+
+    textRes.pop();
+
+    let retStr = ""
+
+    for (let i of textRes) {
+       retStr += i.split('">')[i.split('">').length-1];
+    }
+
+    // REPLACE THIS SHIT LATER
+    retStr = retStr.replace("&amp;#39;", ",");
+
+    return retStr;
 }
