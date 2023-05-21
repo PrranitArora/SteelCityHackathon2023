205164eddd31247283d3946b028723470d40c3c4
ReginaLin2005 <ReginaLin2005@gmail.com>
2023-05-21 15:02:33 -0700

    set up popup for yt shorts

--- Content.js
+++ Content.js
@@ -1,12 +1,26 @@
-const domains = ["www.youtube.com"]
-if(trigger()) alert("active");
+alert("active");
+let popup = document.getElementsByClassName("pop-up");
 
-function trigger() {
-    let triggered = false;
-    console.log("website: " + window.location.hostname)
-    if (domains.includes(window.location.hostname)) {
-        triggered = true;
-    }
-    console.log("triggered: " + triggered);
-    return triggered;
+function openPopup() {
+    popup.classList.add("open-pop-up");
 }
+
+function closePopup() {
+    popup.classList.remove("open-pop-up");
+}
+
+function skip () {
+
+}
+// const domains = ["www.youtube.com/shorts/*"]
+// if(trigger()) alert("active");
+
+// function trigger() {
+//     let triggered = false;
+//     console.log("website: " + window.location.hostname)
+//     if (domains.includes(window.location.hostname)) {
+//         triggered = true;
+//     }
+//     console.log("triggered: " + triggered);
+//     return triggered;
+// }
