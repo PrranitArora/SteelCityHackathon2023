47d57819564a9a662b1cc9d617596089dd2ed5d0
Sidharth R <rsidharth53@gmail.com>
2023-05-22 18:34:03 -0700

    added a model to the server

--- server/server.py
+++ server/server.py
@@ -1,9 +1,14 @@
 from flask import Flask, jsonify, request
+from sentiment_analysis import Analyzer
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    analyzer = Analyzer()
 
-@app.route('/score', methods=['POST'])
-def rate_video():
-    print(request.get_data())
-    #do_something
-    return '', 200
+    @app.route('/score', methods=['POST'])
+    def rate_video():
+        sentiment = analyzer.sentiment(request.json["caption"])
+        return sentiment, 200
+    
+    return app
--- server/server_test.py
+++ server/server_test.py
@@ -1,4 +1,9 @@
 import requests
 
 url = "http://127.0.0.1:5000/score"
-requests.post(url, data={"caption": "abc"})
+while True:
+    text = input("Enter a phrase:\n")
+    if text == "0":
+        break
+    response = requests.post(url, json={"caption": text})
+    print("negative" if response.json() < 0.5 else "positive")
