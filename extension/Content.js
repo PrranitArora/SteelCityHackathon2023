2ddd85402074133699dc9f3ebc1da309f78b1251
Daniel Gorbatov <dzgorbatov@gmail.com>
2023-05-24 16:41:26 -0700

    fuck youtube 2.0

--- extension/Content.js
+++ extension/Content.js
@@ -1,3 +1,5 @@
+let prevPageWasShorts = false;
+
 async function getCaptions(document) {
     console.log(document.body.innerHTML);
 
@@ -28,8 +30,6 @@ async function getCaptions(document) {
     return retStr;
 }
 
-alert("active2");
-
 // // Pause video
 // chrome.tabs.executeScript(tabs[0].id, { code: `
 //     console.log("trying to pause");
@@ -40,29 +40,84 @@ alert("active2");
 //         player.pause();
 //     }, 2000); // time out is a temporary solution since the time it takes for yt to load differs
 // URL event listner
-navigation.addEventListener("navigate", e => {
-    console.log(navigate => e.destination.url)
-  });
 
-// Notification body
-const notification = document.createElement("div");
-notification.className = 'acho-notification';
-notification.innerHTML = 
-    `<h1>Warning</h1>
-    <p>Sensitive content detected. Skip?</p>
-    <button type="button" id="ignore">Ignore</button>
-    <button type="button" id="big-brother-john">Skip</button>`;
+function initExtension() {
+    var extension = document.querySelectorAll(".acho-notification");
+    if (extension.length == 0) {
+        // Notification body
+        const notification = document.createElement("div");
+        notification.className = 'acho-notification';
+        notification.innerHTML = 
+            `<h1>Warning</h1>
+            <p>Sensitive content detected. Skip?</p>
+            <button type="button" id="ignore">Ignore</button>
+            <button type="button" id="big-brother-john">Skip</button>`;
 
-// Add to current page
-document.body.appendChild(notification);
+        // Add to current page
+        document.body.appendChild(notification);
 
-// Button configuration
-document.getElementById("ignore").addEventListener("click", () => {
-    notification.style.display = "none";
-})
+        // Button configuration
+        document.getElementById("ignore").addEventListener("click", () => {
+            notification.style.display = "none";
+        })
 
-document.getElementById("big-brother-john").addEventListener("click", () => {
-    let buttons = document.querySelectorAll('.yt-spec-button-shape-next--mono.yt-spec-button-shape-next--text');
-    var nextButton = buttons[buttons.length - 1];
-    nextButton.click();
-})
+        document.getElementById("big-brother-john").addEventListener("click", () => {
+            console.log("HELLO");
+            let buttons = document.querySelectorAll('.yt-spec-button-shape-next--mono.yt-spec-button-shape-next--text');
+
+            var nextButton; 
+            if (buttons.length === 10 || buttons.length === 4) {
+                nextButton = buttons[3];
+            } else {
+                nextButton = buttons[4];
+            }
+            nextButton.click();
+        })
+    } else {
+        for (let i of extension) {
+            i.style.display = "";
+        } 
+    }
+}
+
+if (window.location.href.includes("www.youtube.com/shorts")) {
+    initExtension();
+}
+
+function closeExtension() {
+    var extension = document.querySelectorAll(".acho-notification");
+
+    for (let i of extension) {
+        i.style.display = "none";
+     }
+}
+
+async function queryApi() {
+    let res = await fetch("localhost:5000/score", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "caption": "INSERT CAPTION" })
+    });
+
+    if (res.ok) {
+        let intRes = await res.text();
+        console.log(intRes);
+    }
+}
+
+navigation.addEventListener("navigate", (e) => {
+    console.log("HELLO");
+    if (e.destination.url.includes("www.youtube.com/shorts")) {
+        if (!prevPageWasShorts) {
+            initExtension();
+        }
+        prevPageWasShorts = true;
+        queryApi();
+    } else {
+        closeExtension();
+        prevPageWasShorts = false;
+    }
+});
